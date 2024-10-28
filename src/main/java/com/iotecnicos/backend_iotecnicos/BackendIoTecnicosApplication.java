894a234567e0bffde38c95f5d5f99921a9099104
Pierix <pierix24@gmail.com>
2024-10-27 23:39:57 -0500

    Primer commit

---
+++ src/main/java/com/iotecnicos/backend_iotecnicos/BackendIoTecnicosApplication.java
@@ -0,0 +1,13 @@
+package com.iotecnicos.backend_iotecnicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendIoTecnicosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendIoTecnicosApplication.class, args);
+    }
+
+}
