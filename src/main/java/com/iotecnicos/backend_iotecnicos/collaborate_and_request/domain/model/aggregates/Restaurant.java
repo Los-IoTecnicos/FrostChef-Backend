4e9c89a953b159b01136705986ff8de4c93a692e
Pierix <89226508+PieroMarquez@users.noreply.github.com>
2024-10-29 23:37:06 -0500

    add: esctructure

---
+++ src/main/java/com/iotecnicos/backend_iotecnicos/collaborate_and_request/domain/model/aggregates/Restaurant.java
@@ -0,0 +1,4 @@
+package com.iotecnicos.backend_iotecnicos.collaborate_and_request.domain.model.aggregates;
+
+public class Restaurant {
+}
---
+++ src/main/java/com/iotecnicos/backend_iotecnicos/collaborate_and_request/domain/model/commands/CreateRestaurantCommand.java
@@ -0,0 +1,4 @@
+package com.iotecnicos.backend_iotecnicos.collaborate_and_request.domain.model.commands;
+
+public record CreateRestaurantCommand() {
+}
