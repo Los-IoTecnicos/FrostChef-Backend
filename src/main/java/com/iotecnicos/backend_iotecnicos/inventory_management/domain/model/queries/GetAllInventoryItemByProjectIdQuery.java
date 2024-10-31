828a5b6574a93092452de6d93c1f741702116725
Pierix <89226508+PieroMarquez@users.noreply.github.com>
2024-10-30 20:50:37 -0500

    feat(inventoryitem): fix command and querys services

--- src/main/java/com/iotecnicos/backend_iotecnicos/inventory_management/domain/model/queries/GetAllInventoryItemByProjectIdQuery.java
+++ src/main/java/com/iotecnicos/backend_iotecnicos/inventory_management/domain/model/queries/GetAllInventoryItemByProjectIdQuery.java
@@ -1,4 +1,6 @@
 package com.iotecnicos.backend_iotecnicos.inventory_management.domain.model.queries;
 
-public record GetAllInventoryItemByProjectIdQuery(Long projectId) {
+import com.iotecnicos.backend_iotecnicos.inventory_management.domain.model.valueobjects.Project;
+
+public record GetAllInventoryItemByProjectIdQuery(Project project) {
 }
